ee80a36b208cd8a62d99e01d28c31909d40db393
gezever <gezever@skynet.be>
2024-10-17 14:46:29 +0200

    demo test

--- src/99_deploy_latest.js
+++ src/99_deploy_latest.js
@@ -1,6 +1,6 @@
 import { deploy_latest } from 'maven-metadata-generator-npm';
 import { set_env } from './utils/setenv.js';
 
-const omgeving = 'pr'
+const omgeving = 'on'
 set_env(omgeving)
 deploy_latest(omgeving)
