f8ff74d2b82aad33634b812647e0b6f373588f72
gezever <gezever@skynet.be>
2025-03-11 17:05:16 +0100

    annotaties

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -96,95 +96,21 @@ const frame_skos_prefixes = {
 
 
 const frame_skos_no_prefixes = {
+    //uri,type,prefLabel,topConceptOf,inScheme,hasTarget,hasBody,motivatedBy,collections
     "@context": context,
-    "@type": ["http://www.w3.org/2004/02/skos/core#ConceptScheme", "http://www.w3.org/2004/02/skos/core#Collection", "http://www.w3.org/2004/02/skos/core#Concept"],
-    "member": {
-        "@type": "http://www.w3.org/2004/02/skos/core#Concept",
+    "@type": ["https://data.omgeving.vlaanderen.be/ns/chemische_stof#Zorgwekkende_stof"],
+    "hasTarget": {
         "@embed": "@never",
         "@omitDefault": true
     },
-    "inScheme": {
-        "@type": "http://www.w3.org/2004/02/skos/core#ConceptScheme",
+    "hasBody": {
         "@embed": "@never",
         "@omitDefault": true
     },
-
-    "topConceptOf": {
-        "@type": "http://www.w3.org/2004/02/skos/core#ConceptScheme",
+    "motivatedBy": {
         "@embed": "@never",
         "@omitDefault": true
     }
-    ,
-    "broader": {
-        "@type": "http://www.w3.org/2004/02/skos/core#Concept",
-        "@embed": "@never",
-        "@omitDefault": true
-    }
-    ,
-    "narrower": {
-        "@type": "http://www.w3.org/2004/02/skos/core#Concept",
-        "@embed": "@never",
-        "@omitDefault": true
-    },
-    "hasTopConcept": {
-        "@type": "http://www.w3.org/2004/02/skos/core#Concept",
-        "@embed": "@never",
-        "@omitDefault": true
-    },
-    "references":{
-        "@embed": "@never",
-        "@omitDefault": true
-    },
-    "isReferencedBy":{
-        "@embed": "@never",
-        "@omitDefault": true
-    },
-    "relation":{
-        "@type": "http://www.w3.org/2004/02/skos/core#Concept",
-        "@embed": "@never",
-        "@omitDefault": true
-    },
-    "broaderTransitive" : {
-        "@type": "http://www.w3.org/2004/02/skos/core#Concept",
-        "@embed": "@never",
-        "@omitDefault": true
-    },
-    "broadMatch" : {
-        "@type": "http://www.w3.org/2004/02/skos/core#Concept",
-        "@embed": "@never",
-        "@omitDefault": true
-    },
-    "closeMatch" : {
-        "@type": "http://www.w3.org/2004/02/skos/core#Concept",
-        "@embed": "@never",
-        "@omitDefault": true
-    },
-    "exactMatch" : {
-        "@type": "http://www.w3.org/2004/02/skos/core#Concept",
-        "@embed": "@never",
-        "@omitDefault": true
-    },
-    "mappingRelation" : {
-        "@type": "http://www.w3.org/2004/02/skos/core#Concept",
-        "@embed": "@never",
-        "@omitDefault": true
-    },
-    "narrowerTransitive" : {
-        "@type": "http://www.w3.org/2004/02/skos/core#Concept",
-        "@embed": "@never",
-        "@omitDefault": true
-    },
-    "narrowMatch" : {
-        "@type": "http://www.w3.org/2004/02/skos/core#Concept",
-        "@embed": "@never",
-        "@omitDefault": true
-    },
-    "semanticRelation" : {
-        "@type": "http://www.w3.org/2004/02/skos/core#Concept",
-        "@embed": "@never",
-        "@omitDefault": true
-    },
-
 }
 
 const ttl = config.skos.path + config.skos.name + '/' + config.skos.name + config.skos.turtle
