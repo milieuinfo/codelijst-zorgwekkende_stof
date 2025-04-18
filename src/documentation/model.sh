12ec4c259076ffa639e83db1d297b58abb00c545
gezever <gezever@skynet.be>
2025-04-18 15:50:22 +0200

    Biociden

--- src/documentation/model.sh
+++ src/documentation/model.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+  #sparql --results=TTL --data=luchtemissie_imjv.ttl  --query model.rq  > model.ttl
   sparql --results=TTL --data=../main/resources/be/vlaanderen/omgeving/data/id/conceptscheme/zorgwekkende_stof/zorgwekkende_stof.ttl  --query model.rq  > model.ttl
   rdf2dot  model.ttl | dot -Tpng > model.png
   rdf2dot  model.ttl  > model.dot
